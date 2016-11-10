a2fda7a33ab3406fcd88aacfde1fa707497e8b50
Sergii <arthasfp@mail.ru>
2016-11-10 08:52:25 +0200

    InMemoryUserMealRepository : repository counter fields

--- src/main/java/ru/javawebinar/topjava/repository/InMemoryUserMealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/InMemoryUserMealRepository.java
@@ -3,11 +3,18 @@ package ru.javawebinar.topjava.repository;
 import ru.javawebinar.topjava.model.UserMeal;
 
 import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by Sergii on 10.11.2016.
  */
 public class InMemoryUserMealRepository implements UserMealRepository {
+    private Map<Integer, UserMeal> repository = new ConcurrentHashMap<>();
+    private AtomicInteger counter = new AtomicInteger(0);
+
+
 
 
     @Override
